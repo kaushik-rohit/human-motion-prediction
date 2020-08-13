25e2700f5d185a440351ced9f9b4b47f7654a4ab
Melvin Ott <melvin.ott@hotmail.com>
2020-08-13 07:33:59 +0200

    most recent submission

--- commonCommands.sh
+++ commonCommands.sh
@@ -48,12 +48,19 @@ bsub -n 6 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" python train.py --data_
 --cell_size 1024 --cell_type blstm --early_stopping_tolerance 200
 
 
-#### current best online score
+
 #1597230977 (valid loss: 2.614; online: 2.128)
 bsub -n 6 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" python train.py --data_dir /cluster/project/infk/hilliges/lectures/mp20/project4 \
 --save_dir ./experiments  --model_type rnn_spl --spl_dropout --spl_dropout_rate 0.0 --input_hidden_size 256 --input_hidden_layers 1 \
 --output_hidden_layers 1 --output_hidden_size 128 --input_dropout_rate 0.05 --num_epochs 1000 --experiment_name rnnspl1024blstm00dropmordecaymoredrop_212891 --learning_rate_decay_rate 0.92 --residual_velocity \
 --cell_size 1024 --cell_type blstm --early_stopping_tolerance 200
+
+#### current best online score
+# 1597264645 (valid loss: 2.619; online: 2.118)
+bsub -n 6 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"  python train.py --data_dir /cluster/project/infk/hilliges/lectures/mp20/project4 \
+--save_dir ./experiments  --model_type rnn_spl --spl_dropout --spl_dropout_rate 0.0 --input_hidden_size 256 --input_hidden_layers 1 \
+--output_hidden_layers 1 --output_hidden_size 128 --input_dropout_rate 0.1 --num_epochs 500 --experiment_name rnnspl1024blstm00dropmordecaymoredrop_212890500 --learning_rate_decay_rate 0.905 --residual_velocity \
+--cell_size 1024 --cell_type blstm --early_stopping_tolerance 60
 
 
 bsub -n 6 -W 4:00 -R "rusage[mem=1024, ngpus_excl_p=1]" -o outvalidate.txt python evaluate_test.py --data_dir /cluster/project/infk/hilliges/lectures/mp20/project4 --save_dir ./experiments --model_id 1597230977 --export
