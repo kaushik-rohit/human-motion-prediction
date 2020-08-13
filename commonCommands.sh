599348798b4324be3d0e68928dc8826ac69060bf
Melvin Ott <melvin.ott@hotmail.com>
2020-08-13 07:44:24 +0200

    rm whitespace

--- commonCommands.sh
+++ commonCommands.sh
@@ -57,7 +57,7 @@ bsub -n 6 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==
 
 #### current best online score
 # 1597264645 (valid loss: 2.619; online: 2.118)
-bsub -n 6 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"  python train.py --data_dir /cluster/project/infk/hilliges/lectures/mp20/project4 \
+bsub -n 6 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" python train.py --data_dir /cluster/project/infk/hilliges/lectures/mp20/project4 \
 --save_dir ./experiments  --model_type rnn_spl --spl_dropout --spl_dropout_rate 0.0 --input_hidden_size 256 --input_hidden_layers 1 \
 --output_hidden_layers 1 --output_hidden_size 128 --input_dropout_rate 0.05 --num_epochs 500 --experiment_name rnnspl1024blstm00dropmordecaymoredrop_2128905 --learning_rate_decay_rate 0.905 --residual_velocity \
 --cell_size 1024 --cell_type blstm --early_stopping_tolerance 60
